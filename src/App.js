1067128210debcc821f05b1d8353581b8ea8bea1
Nritya Kamath <nritya.k@gmail.com>
2019-03-19 21:47:13 -0700

    Using MDL CSS and grid within the library

--- src/App.js
+++ src/App.js
@@ -9,14 +9,17 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header>
-          <nav>
-            <Link to='/'>Bloc Jams </Link>
-            <Link to='/'>Landing</Link>
-            <Link to='/library'>Library</Link>
-          </nav>
-        </header>
+      <div className="demo-layout-transparent mdl-layout mdl-js-layout">
+          <header className="mdl-layout__header mdl-layout__header--transparent">
+            <div className="mdl-layout__header-row">
+              <span className="mdl-layout-title" >Bloc Jams</span>
+              <div className="mdl-layout-spacer"></div>
+              <nav className="mdl-navigation">
+                <Link className="mdl-navigation__link" to='/'>Landing</Link>
+                <Link className="mdl-navigation__link" to='/library'>Library</Link>
+              </nav>
+            </div>
+          </header>
         <main>
           <Route exact path="/" component={Landing} />
           <Route path="/library" component={Library} />
--- src/components/Library.js
+++ src/components/Library.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import albumData from './../data/albums';
+import './Library.css';
+
 
 class Library extends Component {
   constructor(props){
@@ -9,11 +11,11 @@ class Library extends Component {
   }
    render() {
     return (
-      <section className='library'>
+      <section className = 'mdl-grid library'>
         {
           this.state.albums.map((album, index) =>
-            <Link to={`/album/${album.slug}`} key={index}>
-              <img src={album.albumCover} alt={album.title} />
+            <Link className="mdl-cell mdl-cell--4-col" to={`/album/${album.slug}`} key={index}>
+              <img className="photo" src={album.albumCover} alt={album.title} />
               <div>{album.title}</div>
               <div>{album.artist}</div>
               <div>{album.songs.length} songs</div>
